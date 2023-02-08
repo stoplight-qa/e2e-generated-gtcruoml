f7be0b635e0504f79505be0f55604aa8643f3f35
qa-rihea-0-seds@example.com <qa-rihea-0-seds@example.com>
2023-02-08 16:30:14 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15190.stoplight-dev.com/api/v1/projects/cHJqOjE3Mg/spectral.js?branch_slug=main&token=1b994db6-4ef0-469e-9bb4-2096016951c8";
+export default { extends: ruleset };
